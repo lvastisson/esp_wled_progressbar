97a660132338f0d9ea1fabe1c387937ae5d70ee3
lvastisson <94855381+lvastisson@users.noreply.github.com>
2024-05-07 09:09:36 +0300

    optimizations

--- esp_wled_reserv_counter.ino
+++ esp_wled_reserv_counter.ino
@@ -62,19 +62,19 @@ String wled_sync = "http://4.3.2.1:80/win&ST=";
 
 AsyncWebServer server(80);
 
-char buffer[1170];
+char buffer[412];
 
 unsigned long lastTime = 0;
-unsigned long timerDelayMs = 15000;
+unsigned long timerDelayMs = 3000;
 
 unsigned long starttime = millis() / 1000;
 unsigned long localtimee = starttime;
 
 const short ledcount = 300;
-short currled = 0;
-unsigned long counterstart = 1715001174;
-unsigned long reserv = 1715003400;
-unsigned long start_to_reserv_diff = reserv - counterstart;
+short currled = -1;
+unsigned long counterstart = 1715059660;
+unsigned long counterend = 1715074200;
+unsigned long start_to_end_diff = counterend - counterstart;
 
 void time_set(unsigned long time) {
   starttime = millis() / 1000;
@@ -83,9 +83,10 @@ void time_set(unsigned long time) {
 unsigned long time_get() {
   return (millis() / 1000 - starttime + localtimee);
 }
+
 double reservini_progress() {
   double curr_timee = time_get();
-  return 100 - (((double)reserv - curr_timee) / (double)start_to_reserv_diff * (double)100);
+  return 100 - (((double)counterend - curr_timee) / (double)start_to_end_diff * (double)100);
 }
 long led_progress() {
   return (long)((double)(reservini_progress() * (double)ledcount) / (double)100);
@@ -94,7 +95,7 @@ long led_progress() {
 void setup() {
   Serial.begin(115200);
 
-  time_set(1715001174);
+  time_set(1715059660);
 
   WiFi.mode(WIFI_MODE_APSTA);
   WiFi.softAP(soft_ap_ssid, soft_ap_password);
@@ -163,7 +164,7 @@ void setup() {
     if (request->hasParam("t")) {
       inputMessage = request->getParam("t")->value();
       inputParam ="t";
-      reserv = strtoul(inputMessage.c_str(), NULL, 10);
+      counterend = strtoul(inputMessage.c_str(), NULL, 10);
       currled = 0;
     }
     else {
@@ -181,48 +182,49 @@ void setup() {
 
 void loop() {
 
-  if (currled < ledcount && (millis() - lastTime) > timerDelayMs) {
+  if ((millis() - lastTime) > timerDelayMs) {
     if (WiFi.status() == WL_CONNECTED) {
-      neopixelWrite(NEOPIXEL,0,RGB_BRIGHTNESS,0); // Green
-      HTTPClient http;
-
-      http.begin(wled_api.c_str());
-      http.addHeader("Content-Type", "application/json");
-
-
       short curr_progress = led_progress();
-      Serial.print("Set progress to n-th led: ");
-      Serial.println(curr_progress);
 
-      // compile string template
-      snprintf(buffer, 1170, "{\"on\":true,\"bri\":255,\"transition\":7,\"tt\":50,\"mainseg\":0,\"seg\":[{\"id\":0,\"start\":%d,\"stop\":300,\"grp\":1,\"spc\":0,\"of\":0,\"on\":true,\"frz\":false,\"bri\":255,\"cct\":127,\"set\":0,\"n\":\"\",\"col\":[[255,160,0],[0,0,0],[0,0,0]],\"fx\":106,\"sx\":0,\"ix\":255,\"pal\":18,\"c1\":128,\"c2\":128,\"c3\":16,\"sel\":false,\"rev\":false,\"mi\":false,\"o1\":false,\"o2\":false,\"o3\":false,\"si\":0,\"m12\":0},{\"id\":1,\"start\":0,\"stop\":%d,\"grp\":1,\"spc\":0,\"of\":0,\"on\":true,\"frz\":false,\"bri\":194,\"cct\":127,\"set\":0,\"n\":\"\",\"col\":[[0,255,64],[69,255,13],[179,255,0]],\"fx\":67,\"sx\":15,\"ix\":255,\"pal\":4,\"c1\":128,\"c2\":128,\"c3\":16,\"sel\":true,\"rev\":true,\"mi\":false,\"o1\":false,\"o2\":false,\"o3\":false,\"si\":0,\"m12\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0},{\"stop\":0}]}", curr_progress, curr_progress);
-      // Send HTTP POST request
-      int httpResponseCode = http.POST(String(buffer));
+      if (currled != curr_progress) {
+        neopixelWrite(NEOPIXEL,0,RGB_BRIGHTNESS,0); // Green
+        HTTPClient http;
 
-      if (httpResponseCode > 0) {
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-        String payload = http.getString();
-        Serial.println(payload);
-      } else {
-        Serial.print("Error code: ");
-        Serial.println(httpResponseCode);
+        http.begin(wled_api.c_str());
+        http.addHeader("Content-Type", "application/json");
+
+
+        Serial.print("Set progress to n-th led: ");
+        Serial.println(curr_progress);
+
+        // compile string template
+        snprintf(buffer, 412, "{\"mainseg\":0,\"seg\":[{\"id\":0,\"start\":%d,\"stop\":300},{\"id\":1,\"start\":0,\"stop\":%d,\"grp\":1,\"spc\":0,\"of\":0,\"on\":true,\"frz\":false,\"bri\":194,\"cct\":127,\"set\":0,\"n\":\"\",\"col\":[[0,255,64],[69,255,13],[179,255,0]],\"fx\":67,\"sx\":15,\"ix\":255,\"pal\":4,\"c1\":128,\"c2\":128,\"c3\":16,\"rev\":true,\"mi\":false,\"o1\":false,\"o2\":false,\"o3\":false,\"si\":0,\"m12\":0}]}", curr_progress, curr_progress);
+
+        // Send HTTP POST request
+        int httpResponseCode = http.POST(String(buffer));
+
+        if (httpResponseCode > 0) {
+          Serial.print("HTTP Response code: ");
+          Serial.println(httpResponseCode);
+          String payload = http.getString();
+          Serial.println(payload);
+          currled = curr_progress;
+        } else {
+          Serial.print("Error code: ");
+          Serial.println(httpResponseCode);
+        }
+
+        // Free resources
+        http.end();
       }
-      
-
-      // Free resources
-      http.end();
-
-      currled = curr_progress;
     } else {
-      neopixelWrite(NEOPIXEL,RGB_BRIGHTNESS,0,0); // Red
-      delay(250);
-      neopixelWrite(NEOPIXEL,0,0,0); // Off / black
-      delay(250);
+      neopixelWrite(NEOPIXEL,138,70,170); // Purple
       Serial.println("Reconnecting to WiFi...");
       WiFi.disconnect();
+      delay(500);
       WiFi.reconnect();
     }
+
     lastTime = millis();
   }
 }
